cd15a195f99aff79e7b2d552d46ae9c91f0daa78
Adam Jazairi <adamjazairi@gmail.com>
2017-03-28 13:02:49 -0400

    Delete pesky auto-generated rights statements

---
+++ sql/clear_rights_statements.sql
@@ -0,0 +1,5 @@
+/* The migration plugin interprets rightsTransferred="True" as a copyright statement, so we need to clear
+the rights_statement table after migration. At the time of migration table contains only these incorrectly generated
+copyright statements. */
+
+DELETE FROM rights_statement;
