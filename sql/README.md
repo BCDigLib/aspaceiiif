9d5f5c1c6f1ed8883e15998d52876054c08546b1
Adam Jazairi <adamjazairi@gmail.com>
2017-04-21 11:29:33 -0400

    Remove SQL statements to avoid confusion

--- sql/README.md
+++
@@ -1,1 +0,0 @@
-These SQL queries were used to normalize data post-migration. They are specific to our use case and are here for reference purposes only.
--- sql/external_location.sql
+++
@@ -1,4 +0,0 @@
-/* This statement removes the 'http://' prefix from the location field of
-digital objects. */
-
-UPDATE external_document SET location = REPLACE(location,'http://','');
