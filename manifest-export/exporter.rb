ff1b0df566076e8d3979e24117b7c5042f9d0e0b
Adam Jazairi <adamjazairi@gmail.com>
2018-06-13 12:08:23 -0400

    Add methods to fetch digital object and archival object JSON

--- manifest-export/exporter.rb
+++ manifest-export/exporter.rb
@@ -16,4 +16,20 @@ class ManifestExporter
     auth_resp_serialized = JSON.parse(auth_resp)
     @session_id = auth_resp_serialized["session"]
   end
+
+  def get_digital_object(input)
+    digital_object_base_uri = @conf["aspace_base_uri"] + '/repositories/2/digital_objects/'
+    endpoint = digital_object_base_uri + input
+    response = RestClient.get(endpoint, {"X-ArchivesSpace-Session": @session_id})
+    digital_object = JSON.parse(response)
+  end
+
+  def get_archival_object(input)
+    archival_object_base_uri = @conf["aspace_base_uri"] + '/repositories/2/archival_objects/'
+    endpoint = archival_object_base_uri + input
+    response = RestClient.get(endpoint, {"X-ArchivesSpace-Session": @session_id})
+    archival_object = JSON.parse(response)
+  end
+
+
 end
