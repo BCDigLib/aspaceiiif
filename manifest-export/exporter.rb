332e040e48605ed24232606b982b3cf41798b19d
Adam Jazairi <adamjazairi@gmail.com>
2018-06-13 11:29:14 -0400

    Refactor into OOP

--- manifest-export/exporter.rb
+++ manifest-export/exporter.rb
@@ -3,11 +3,17 @@ require 'rest-client'
 require 'json'
 require 'yaml'
 
-conf = YAML::load_file('config.yml')
-auth_resp = RestClient::Request.execute(method: :post, 
-                                       url: conf["aspace_base_uri"] + '/users/admin/login',
-                                       payload: {password: conf["aspace_password"]}
-)
-auth_resp_serialized = JSON.parse(auth_resp)
-session_id = auth_resp_serialized["session"]
+class ManifestExporter
+  def initialize(cnf_file)
+    @conf = YAML::load_file(cnf_file)
+  end
 
+  def authenticate
+    auth_resp = RestClient::Request.execute(method: :post, 
+                                           url: @conf["aspace_base_uri"] + '/users/admin/login',
+                                           payload: {password: @conf["aspace_password"]}
+    )
+    auth_resp_serialized = JSON.parse(auth_resp)
+    @session_id = auth_resp_serialized["session"]
+  end
+end
