8a8fdf3aea886a652c4d7311945910e0a9f26015
Adam Jazairi <adamjazairi@gmail.com>
2018-06-14 11:04:41 -0400

    Start on build_manifest method

--- manifest-export/exporter.rb
+++ manifest-export/exporter.rb
@@ -65,7 +65,15 @@ module ManifestExport
     def export_manifest
     end
 
-    def build_manifest
+    def build_manifest(dig_obj_id)
+      as_records = Records.new(dig_obj_id)
+      digital_object = as_records.digital_object
+      digital_object_tree = as_records.digital_object_tree
+      archival_object = as_records.archival_object
+      resource = as_records.resource
+
+      handle = digital_object["digital_object_id"]
+      rights_statement = digital_object["notes"].select { |note| note["type"] == "userestrict" }[0]["content"][0]
     end
 
     def generate_annotation
