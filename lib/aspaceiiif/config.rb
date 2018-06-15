a6c9b952be1c65a7b66f567276b9d3d9ae091355
Adam Jazairi <adamjazairi@gmail.com>
2018-06-15 13:47:37 -0400

    Refactor Config class

--- lib/aspaceiiif/config.rb
+++ lib/aspaceiiif/config.rb
@@ -2,12 +2,8 @@ require 'yaml'
 
 module Aspaceiiif
   class Config
-    def self.root
-      File.dirname 'aspace-to-iiif'
-    end
-
     def self.load
-      conf = YAML::load_file(File.join(root, 'config.yml'))
+      YAML::load_file(File.join(__dir__, '../../config.yml'))
     end
   end
 end
