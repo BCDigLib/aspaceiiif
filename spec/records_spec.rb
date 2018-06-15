e41c97fde7964cb7f0120700d6d884960b57ab35
Adam Jazairi <adamjazairi@gmail.com>
2018-06-15 13:56:46 -0400

    Add context block

--- spec/records_spec.rb
+++ spec/records_spec.rb
@@ -3,27 +3,29 @@ require 'aspaceiiif/records'
 describe Aspaceiiif::Records do
   let(:records) { Aspaceiiif::Records.new('1596') }
 
-  describe "#digital_object" do
-    it "returns digital object JSON" do
-      expect(records.digital_object["jsonmodel_type"]).to eq "digital_object"
+  context "given an instance of the Records class" do
+    describe "#digital_object" do
+      it "returns digital object JSON" do
+        expect(records.digital_object["jsonmodel_type"]).to eq "digital_object"
+      end
     end
-  end
 
-  describe "#digital_object_tree" do
-    it "returns digital object tree JSON" do
-      expect(records.digital_object_tree["jsonmodel_type"]).to eq "digital_object_tree"
+    describe "#digital_object_tree" do
+      it "returns digital object tree JSON" do
+        expect(records.digital_object_tree["jsonmodel_type"]).to eq "digital_object_tree"
+      end
     end
-  end
 
-  describe "#archival_object" do
-    it "returns archival object JSON" do
-      expect(records.archival_object["jsonmodel_type"]).to eq "archival_object"
+    describe "#archival_object" do
+      it "returns archival object JSON" do
+        expect(records.archival_object["jsonmodel_type"]).to eq "archival_object"
+      end
     end
-  end
 
-  describe "#resource" do
-    it "returns resource JSON" do
-      expect(records.resource["jsonmodel_type"]).to eq "resource"
+    describe "#resource" do
+      it "returns resource JSON" do
+        expect(records.resource["jsonmodel_type"]).to eq "resource"
+      end
     end
   end
 end
