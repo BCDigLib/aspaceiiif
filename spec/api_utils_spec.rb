685a7dd8a374b9ebd7422425cfd6dc32c4134a04
Adam Jazairi <adamjazairi@gmail.com>
2018-06-15 12:27:37 -0400

    Write APIUtils test

--- spec/api_utils_spec.rb
+++ spec/api_utils_spec.rb
@@ -2,7 +2,13 @@ require 'aspaceiiif/api_utils'
 
 describe Aspaceiiif::APIUtils do
 
-  describe ".get_record" do
-    context ""
+  describe "#get_record(uri_suffix)" do
+    let(:conn) { Aspaceiiif::APIUtils.new }
+
+    context "given a valid endpoint" do
+      it "returns a hash" do
+        expect(conn.get_record('/repositories/2/digital_objects/1596')).to be_a(Hash)
+      end
+    end
   end
 end
