cc1317412cc958dae471ca7a0a4ab317c331f825
Adam Jazairi <adamjazairi@gmail.com>
2018-06-15 13:36:00 -0400

    Add tests for Records class

--- spec/records_spec.rb
+++ spec/records_spec.rb
@@ -1,4 +1,29 @@
 require 'aspaceiiif/records'
 
 describe Aspaceiiif::Records do
+  let(:records) { Aspaceiiif::Records.new('1596') }
+
+  describe "#digital_object" do
+    it "returns digital object JSON" do
+      expect(records.digital_object["jsonmodel_type"]).to eq "digital_object"
+    end
+  end
+
+  describe "#digital_object_tree" do
+    it "returns digital object tree JSON" do
+      expect(records.digital_object_tree["jsonmodel_type"]).to eq "digital_object_tree"
+    end
+  end
+
+  describe "#archival_object" do
+    it "returns archival object JSON" do
+      expect(records.archival_object["jsonmodel_type"]).to eq "archival_object"
+    end
+  end
+
+  describe "#resource" do
+    it "returns resource JSON" do
+      expect(records.resource["jsonmodel_type"]).to eq "resource"
+    end
+  end
 end
