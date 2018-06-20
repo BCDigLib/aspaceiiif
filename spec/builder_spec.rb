ef20c79b890407492f74dcad9a2c9fc58be6f40a
Adam Jazairi <adamjazairi@gmail.com>
2018-06-20 11:16:48 -0400

    Remove obsolete test

--- spec/builder_spec.rb
+++ spec/builder_spec.rb
@@ -1,13 +1,4 @@
 require 'aspaceiiif/builder'
 
 describe ASpaceIIIF::Builder do
-  let(:builder) { ASpaceIIIF::Builder.new }
-
-  describe "#build_manifest(dig_obj_id)" do
-    context "given a digital object PK" do
-      it "finds a handle in the digital_object_id property" do
-        expect(builder.build_manifest('1596')["digital_object_id"]).to include? "hdl.handle.net"
-      end
-    end
-  end
 end
