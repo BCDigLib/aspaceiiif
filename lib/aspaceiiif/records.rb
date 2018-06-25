41fad42816af319cd0b0863351c49145ea1ee191
Adam Jazairi <adamjazairi@gmail.com>
2018-06-25 13:39:09 -0400

    Pass tests for Records#digital_object_components

--- lib/aspaceiiif/records.rb
+++ lib/aspaceiiif/records.rb
@@ -17,6 +17,11 @@ module ASpaceIIIF
       @conn.get_record(tree_id)
     end
 
+    def digital_object_components
+      digital_object_component_ids = digital_object_tree["children"].map { |e| e["record_uri"] }
+      digital_object_component_ids.map { |uri| @conn.get_record(uri) }
+    end
+
     def archival_object
       arch_obj_id = digital_object["linked_instances"][0]["ref"]
       @conn.get_record(arch_obj_id)
