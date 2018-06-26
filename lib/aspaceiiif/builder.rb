15dd5faeb3b2c044301668bacb597440a52ffcb6
Adam Jazairi <adamjazairi@gmail.com>
2018-06-26 16:16:35 -0400

    Pass generator tests

--- lib/aspaceiiif/builder.rb
+++ lib/aspaceiiif/builder.rb
@@ -30,16 +30,56 @@ module ASpaceIIIF
       IIIF::Presentation::Manifest.new(seed)
     end
 
-    def generate_annotation
+    def generate_canvas(image_file, label, order)
+      separator = image_file.include?('_') ? '_' : '.'
+      image_id = image_file.chomp('.jp2').chomp('.tif').chomp('.tiff').chomp('.jpg')
+      page_id = image_id.split(separator).last
+
+      canvas_id = "#{@sequence_base}/canvas/#{page_id}"
+
+      annotation_seed = {
+          '@id' => "#{canvas_id}/annotation/1",
+          'on' => canvas_id
+      }
+      annotation = IIIF::Presentation::Annotation.new(annotation_seed)
+      annotation.resource = generate_image_resource(image_file)
+
+      canvas_seed = {
+          '@id' => canvas_id,
+          'label' => label,
+          'width' => annotation.resource['width'],
+          'height' => annotation.resource['height'],
+          'images' => [annotation]
+      }
+      IIIF::Presentation::Canvas.new(canvas_seed)
     end
 
-    def generate_image_resource
+    def generate_image_resource(image_file)
+      base_uri = "#{@image_server}/#{image_file}"
+      image_api_params = '/full/full/0/default.jpg'
+
+      params = {
+          service_id: base_uri,
+          resource_id_default: "#{base_uri}#{image_api_params}",
+          resource_id: "#{base_uri}#{image_api_params}"
+      }
+      IIIF::Presentation::ImageResource.create_image_api_image_resource(params)
     end
 
-    def build_canvas
-    end
+    def generate_range(image_file, label, order)
+      separator = image_file.include?('_') ? '_' : '.'
+      image_id = image_file.chomp('.jp2').chomp('.tif').chomp('.tiff').chomp('.jpg')
+      page_id = image_id.split(separator).last
 
-    def build_range
+      range_id = "#{@sequence_base}/range/r-#{order}"
+      canvas_id = "#{@sequence_base}/canvas/#{page_id}"
+
+      seed = {
+          '@id' => range_id,
+          'label' => label,
+          'canvases' => [canvas_id]
+      }
+      IIIF::Presentation::Range.new(seed)
     end
   end
 end
