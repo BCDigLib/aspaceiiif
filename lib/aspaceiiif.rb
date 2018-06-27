94233a57bf65639c4289012f81672e6a878d67ba
Adam Jazairi <adamjazairi@gmail.com>
2018-06-27 10:43:07 -0400

    And Builder instantiation and manifest output to ASpaceIIIF#run

--- lib/aspaceiiif.rb
+++ lib/aspaceiiif.rb
@@ -1,6 +1,13 @@
 require 'aspaceiiif/version'
+require 'aspaceiiif/builder'
 
 module ASpaceIIIF
-  def run
+  def self.run    
+    dig_obj_id = ARGV[0]
+
+    builder = ASpaceIIIF::Builder.new(dig_obj_id)
+    manifest = builder.generate_manifest
+
+    puts manifest.to_json(pretty: true)
   end
 end
