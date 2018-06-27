e079997620f649fb7557fb1d4254f22d6e097d07
Adam Jazairi <adamjazairi@gmail.com>
2018-06-27 10:44:35 -0400

    Add opt parser

--- lib/aspaceiiif.rb
+++ lib/aspaceiiif.rb
@@ -2,7 +2,16 @@ require 'aspaceiiif/version'
 require 'aspaceiiif/builder'
 
 module ASpaceIIIF
-  def self.run    
+  def self.run
+    OptionParser.new do |parser|
+      parser.banner = "Usage: aspaceiiif dig_obj_id"
+
+      parser.on("-h", "--help", "Show this help message") do ||
+        puts parser
+        exit
+      end
+    end.parse!
+
     dig_obj_id = ARGV[0]
 
     builder = ASpaceIIIF::Builder.new(dig_obj_id)
