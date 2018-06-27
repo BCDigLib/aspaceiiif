d7975110dddcb0215f4659839788af879e8628bb
Adam Jazairi <adamjazairi@gmail.com>
2018-06-27 09:40:42 -0400

    Further clarify test variable names

--- spec/builder_spec.rb
+++ spec/builder_spec.rb
@@ -5,66 +5,66 @@ describe ASpaceIIIF::Builder do
   let(:metadata) { ASpaceIIIF::Metadata.new('1596') }
 
   describe "#export_manifest" do
-    let(:manifest) { builder.export_manifest }
+    let(:manifest_export) { builder.export_manifest }
 
     it "outputs a manifest" do
-      expect(manifest["@type"]).to eq("sc:Manifest")
+      expect(manifest_export["@type"]).to eq("sc:Manifest")
     end
 
     it "conforms to v2 of the IIIF presentation API" do
-      expect(manifest["@context"]).to eq("http://iiif.io/api/presentation/2/context.json")
+      expect(manifest_export["@context"]).to eq("http://iiif.io/api/presentation/2/context.json")
     end
 
     it "includes a sequences block" do
-      expect(manifest["sequences"]).to be_instance_of(Array)
-      expect(manifest["sequences"].length).to be > 0
+      expect(manifest_export["sequences"]).to be_instance_of(Array)
+      expect(manifest_export["sequences"].length).to be > 0
     end
 
     it "contains at least one canvas in the sequences block" do
-      expect(manifest["sequences"][0]["@type"]).to eq("sc:Canvas")
+      expect(manifest_export["sequences"][0]["@type"]).to eq("sc:Canvas")
     end
 
     it "includes a structures block" do
-      expect(manifest["structures"]).to be_instance_of(Array)
-      expect(manifest["sequences"].length).to be > 0 
+      expect(manifest_export["structures"]).to be_instance_of(Array)
+      expect(manifest_export["sequences"].length).to be > 0 
     end
 
     it "contains at least one range in the structures block" do
-      expect(manifest["structures"][0]["@type"]).to eq("sc:Range")
+      expect(manifest_export["structures"][0]["@type"]).to eq("sc:Range")
     end
   end
 
   describe "#generate_manifest" do
-    let(:base_manifest) { builder.generate_manifest(metadata) }
+    let(:manifest_seed) { builder.generate_manifest(metadata) }
 
     it "has a valid ID attribute" do
-      expect(base_manifest["@id"]).to be_instance_of(String)
-      expect(base_manifest["@id"]).to match(/https:\/\/library\.bc\.edu\/(BC)\d{4}_\d{3}_.+\.json|(MS)\d{4}_\d{3}_.+\.json/)
+      expect(manifest_seed["@id"]).to be_instance_of(String)
+      expect(manifest_seed["@id"]).to match(/https:\/\/library\.bc\.edu\/(BC)\d{4}_\d{3}_.+\.json|(MS)\d{4}_\d{3}_.+\.json/)
     end
 
     it "contains the record title" do
-      expect(base_manifest["label"]).to eq(metadata.title)
+      expect(manifest_seed["label"]).to eq(metadata.title)
     end
 
     it "includes rights information in the attribution attribute" do
-      expect(base_manifest["attribution"]).to eq(metadata.rights_statement)
+      expect(manifest_seed["attribution"]).to eq(metadata.rights_statement)
     end
 
     it "contains a metadata block" do
-      expect(base_manifest["metadata"]).to be_instance_of(Array)
-      expect(base_manifest["metadata"].length).to be > 0
+      expect(manifest_seed["metadata"]).to be_instance_of(Array)
+      expect(manifest_seed["metadata"].length).to be > 0
     end
 
     it "includes a handle in the metadata block" do
-      expect(base_manifest["metadata"][0][:handle]).to include("hdl.handle.net")
+      expect(manifest_seed["metadata"][0][:handle]).to include("hdl.handle.net")
     end
 
     it "includes a preferred citation in the metadata block" do
-      expect(base_manifest["metadata"][1][:label]).to eq("Preferred Citation")
+      expect(manifest_seed["metadata"][1][:label]).to eq("Preferred Citation")
     end
 
     it "includes all expected values in the preferred citation" do
-      expect(base_manifest["metadata"][1][:value]).to include(metadata.title && metadata.resource_id && metadata.owner && metadata.handle)
+      expect(manifest_seed["metadata"][1][:value]).to include(metadata.title && metadata.resource_id && metadata.owner && metadata.handle)
     end
   end
 
