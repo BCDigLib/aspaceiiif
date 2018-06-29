456a1cf7796d7b760a0ed81d9a1a483d8db46d3c
Adam Jazairi <adamjazairi@gmail.com>
2018-06-29 10:22:47 -0400

    Begin updating Records methods to accommodate resource ID as input

--- lib/aspaceiiif/records.rb
+++ lib/aspaceiiif/records.rb
@@ -2,34 +2,41 @@ require 'aspaceiiif/api_utils'
 
 module ASpaceIIIF
   class Records
-    def initialize(dig_obj_id)
-      @dig_obj_id = dig_obj_id
+    def initialize(resource_id)
+      @resource_id = resource_id
       @conn = APIUtils.new
     end
 
-    def digital_object
-      dig_obj_ref = '/repositories/2/digital_objects/' + @dig_obj_id
-      @conn.get_record(dig_obj_ref)
+    def resource
+      resource_ref = '/repositories/2/digital_objects/' + @resource_id
+      @conn.get_record(resource_ref)
     end
 
-    def digital_object_tree
-      dig_obj_tree_ref = digital_object["tree"]["ref"]
-      @conn.get_record(dig_obj_tree_ref)
+    def resource_tree
+      resource_tree_ref = resource["tree"]["ref"]
+      @conn.get_record(resource_tree_ref)
+    end
+
+    def archival_objects
+      resource_components = resource_tree["children"].select { |e| e["children"].any? { |e| e["instance_types"].include?("digital_object") } }[0]["children"]
+      arch_obj_refs = resource_components.map { |obj| obj["record_uri"] }
+      arch_obj_refs.map { |ref| @conn.get_record(ref) }
+    end
+
+    def digital_objects
+      dig_obj_instances = archival_objects.map { |obj| obj["instances"].detect { |instance| instance["instance_type"] == "digital_object" } }.compact
+      dig_obj_refs = dig_obj_instances.map { |instance| instance["digital_object"]["ref"] }
+      dig_obj_refs.map { |ref| @conn.get_record(ref) }
+    end
+
+    def digital_object_trees
+      dig_obj_tree_refs = digital_objects.map { |obj| obj["tree"]["ref"] }
+      dig_obj_tree_refs.map { |ref| @conn.get_record(ref) }
     end
 
     def digital_object_components
-      dig_obj_comp_refs = digital_object_tree["children"].map { |e| e["record_uri"] }
-      dig_obj_comp_refs.map { |uri| @conn.get_record(uri) }
-    end
-
-    def archival_object
-      arch_obj_ref = digital_object["linked_instances"][0]["ref"]
-      @conn.get_record(arch_obj_ref)
-    end
-
-    def resource
-      resource_ref = archival_object["resource"]["ref"]
-      @conn.get_record(resource_ref)
+      dig_obj_comp_refs = digital_object_trees.map { |tree| tree["children"].map { |e| e["record_uri"] } }
+      dig_obj_comp_refs.map { |ref| @conn.get_record(ref) }
     end
 
     def linked_agent
