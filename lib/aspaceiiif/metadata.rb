3adfee97940e14514dbb61260f02d172cbc03456
Adam Jazairi <adamjazairi@gmail.com>
2018-06-29 11:16:25 -0400

    Refactor Metadata methods to reflect changes to Records class

--- lib/aspaceiiif/metadata.rb
+++ lib/aspaceiiif/metadata.rb
@@ -4,27 +4,27 @@ module ASpaceIIIF
   class Metadata
     def initialize(dig_obj_id)
       as_records = Records.new(dig_obj_id)
-      @digital_object = as_records.digital_object
-      @digital_object_tree = as_records.digital_object_tree
+      @digital_objects = as_records.digital_objects
+      @digital_object_trees = as_records.digital_object_trees
       @digital_object_components = as_records.digital_object_components
-      @archival_object = as_records.archival_object
+      @archival_objects = as_records.archival_objects
       @resource = as_records.resource
-      @linked_agent = as_records.linked_agent
+      @linked_agents = as_records.linked_agents
     end
 
     def handle 
-      @digital_object["digital_object_id"]
+      @digital_objects.map { |record| record["digital_object_id"] }
     end
 
     def rights_statement
-      @digital_object["notes"].select { |note| note["type"] == "userestrict" }[0]["content"][0]
+      @digital_objects.map { |record| record["notes"].select { |note| note["type"] == "userestrict" }[0]["content"][0] }
     end
 
     def title
       if host_title.nil?
-        @digital_object["title"]
+        @digital_objects.map { |record| record["title"] }
       else
-        @digital_object["title"] + ", " + host_title
+        @digital_objects.map { |record| record["title"] + ", " + host_title }
       end
     end
 
@@ -37,11 +37,11 @@ module ASpaceIIIF
     end
 
     def component_id
-      @archival_object["component_id"]
+      @archival_objects.map { |record| record["component_id"] }
     end
 
     def creator
-      @linked_agent["title"]
+      @linked_agents.map { |record| record["title"] }
     end
 
     def owner
