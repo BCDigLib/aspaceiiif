f6816396a12bd4354b4ef0b8c7d1c79ca3e3b598
Adam Jazairi <16103405+jazairi@users.noreply.github.com>
2018-08-07 16:49:27 -0400

    Revert Records class to handle digital object input

--- lib/aspaceiiif/records.rb
+++ lib/aspaceiiif/records.rb
@@ -2,44 +2,44 @@ require 'aspaceiiif/api_utils'
 
 module ASpaceIIIF
   class Records
-    def initialize(resource_id)
-      @resource_id = resource_id
+    def initialize(dig_obj_id)
+      @dig_obj_id = dig_obj_id
       @conn = APIUtils.new
     end
 
     def digital_object
-      uri_suffix = '/repositories/2/digital_objects/' + @dig_obj_id
-      @conn.get_record(uri_suffix)
+      dig_obj_ref = '/repositories/2/digital_objects/' + @dig_obj_id
+      @conn.get_record(dig_obj_ref)
     end
 
     def digital_object_tree
-      tree_id = digital_object["tree"]["ref"]
-      @conn.get_record(tree_id)
+      dig_obj_tree_ref = digital_object["tree"]["ref"]
+      @conn.get_record(dig_obj_tree_ref)
     end
 
     def digital_object_components
-      digital_object_component_ids = digital_object_tree["children"].map { |e| e["record_uri"] }
-      digital_object_component_ids.map { |uri| @conn.get_record(uri) }
+      dig_obj_comp_refs = digital_object_tree["children"].map { |e| e["record_uri"] }
+      dig_obj_comp_refs.map { |uri| @conn.get_record(uri) }
     end
 
     def archival_object
-      arch_obj_id = digital_object["linked_instances"][0]["ref"]
-      @conn.get_record(arch_obj_id)
+      arch_obj_ref = digital_object["linked_instances"][0]["ref"]
+      @conn.get_record(arch_obj_ref)
     end
 
     def resource
-      resource_id = archival_object["resource"]["ref"]
-      @conn.get_record(resource_id)
+      resource_ref = archival_object["resource"]["ref"]
+      @conn.get_record(resource_ref)
     end
 
     def linked_agent
       unless (archival_object["linked_agents"].empty? && resource["linked_agents"].empty?)
         if archival_object["linked_agents"].length > 0 
-          agent_id = archival_object["linked_agents"].detect { |e| e["role"] == "creator" }["ref"]
-          @conn.get_record(agent_id)
+          agent_ref = archival_object["linked_agents"].detect { |e| e["role"] == "creator" }["ref"]
+          @conn.get_record(agent_ref)
         else
-          agent_id = resource["linked_agents"].detect { |e| e["role"] == "creator" }["ref"]
-          @conn.get_record(agent_id)
+          agent_ref = resource["linked_agents"].detect { |e| e["role"] == "creator" }["ref"]
+          @conn.get_record(agent_ref)
         end
       end
     end
