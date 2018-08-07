b9123de8ae991956f7062bc0fd72f271f792d6d6
Adam Jazairi <16103405+jazairi@users.noreply.github.com>
2018-08-07 16:42:15 -0400

    Revert "Update Records variable names"
    This reverts commit b009d2871ceb40fef62af09e7577e8bd4e363368. We're
    moving these changes to a separate branch, resource_id, following
    discussions that digital_object_id should remain the input value.

--- lib/aspaceiiif/records.rb
+++ lib/aspaceiiif/records.rb
@@ -7,85 +7,41 @@ module ASpaceIIIF
       @conn = APIUtils.new
     end
 
-    def resource
-      resource_ref = '/repositories/2/digital_objects/' + @resource_id
-      @conn.get_record(resource_ref)
+    def digital_object
+      uri_suffix = '/repositories/2/digital_objects/' + @dig_obj_id
+      @conn.get_record(uri_suffix)
     end
 
-    def resource_tree
-      resource_tree_ref = resource["tree"]["ref"]
-      @conn.get_record(resource_tree_ref)
-    end
-
-    def archival_objects
-      # Ugly but necessary, since the JSONPath Ruby implementation doesn't fully 
-      # support ?(): https://github.com/joshbuddy/jsonpath/issues/54
-      resource_children = resource_tree["children"].each do |child|
-        if child["instance_types"].include?("digital_object")
-          puts child
-        elsif child["children"].length > 0
-          child["children"].each do |child|
-            if child["instance_types"].include?("digital_object")
-              puts child
-            elsif child["children"].length > 0
-              child["children"].each do |child|
-                if child["instance_types"].include?("digital_object")
-                  puts child
-                elsif child["children"].length > 0
-                  child["children"].each do |child|
-                    if child["instance_types"].include?("digital_object")
-                      puts child
-                    elsif child["children"].length > 0
-                      child["children"].each do |child|
-                        if child["instance_types"].include?("digital_object")
-                          puts child
-                        end
-                      end
-                    end
-                  end
-                end
-              end
-            end
-          end 
-        end
-      end
-
-      arch_obj_refs = resource_children.map { |obj| obj["record_uri"] }
-      arch_obj_refs.map { |ref| @conn.get_record(ref) }
-    end
-
-    def digital_objects
-      dig_obj_instances = archival_objects.map { |obj| obj["instances"].detect { |instance| instance["instance_type"] == "digital_object" } }.compact
-      dig_obj_refs = dig_obj_instances.map { |instance| instance["digital_object"]["ref"] }
-      dig_obj_refs.map { |ref| @conn.get_record(ref) }
-    end
-
-    def digital_object_trees
-      dig_obj_tree_refs = digital_objects.map { |obj| obj["tree"]["ref"] }
-      dig_obj_tree_refs.map { |ref| @conn.get_record(ref) }
+    def digital_object_tree
+      tree_id = digital_object["tree"]["ref"]
+      @conn.get_record(tree_id)
     end
 
     def digital_object_components
-      dig_obj_comp_refs = digital_object_trees.map { |tree| tree["children"].map { |e| e["record_uri"] } }
-      dig_obj_comp_refs.map { |ref| @conn.get_record(ref) }
+      digital_object_component_ids = digital_object_tree["children"].map { |e| e["record_uri"] }
+      digital_object_component_ids.map { |uri| @conn.get_record(uri) }
     end
 
-    def linked_agents
-      agent_records = {}
+    def archival_object
+      arch_obj_id = digital_object["linked_instances"][0]["ref"]
+      @conn.get_record(arch_obj_id)
+    end
 
-      archival_objects.each do |obj|
-        unless (obj["linked_agents"].empty? && resource["linked_agents"].empty?)
-          if obj["linked_agents"].length > 0 
-            agent_ref = obj["linked_agents"].detect { |e| e["role"] == "creator" }["ref"]
-            agent_records["#{obj[uri]}"] = @conn.get_record(agent_ref)
-          else
-            agent_ref = resource["linked_agents"].detect { |e| e["role"] == "creator" }["ref"]
-            agent_records["#{obj[uri]}"] = @conn.get_record(agent_ref)
-          end
+    def resource
+      resource_id = archival_object["resource"]["ref"]
+      @conn.get_record(resource_id)
+    end
+
+    def linked_agent
+      unless (archival_object["linked_agents"].empty? && resource["linked_agents"].empty?)
+        if archival_object["linked_agents"].length > 0 
+          agent_id = archival_object["linked_agents"].detect { |e| e["role"] == "creator" }["ref"]
+          @conn.get_record(agent_id)
+        else
+          agent_id = resource["linked_agents"].detect { |e| e["role"] == "creator" }["ref"]
+          @conn.get_record(agent_id)
         end
       end
-
-      agent_records
     end
   end
 end
