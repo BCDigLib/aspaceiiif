e23d5896826dc1cd6131eb5b8f111f520a7d672c
Adam Jazairi <16103405+jazairi@users.noreply.github.com>
2018-08-07 16:47:54 -0400

    Revert Metadata class to handle digital object input

--- lib/aspaceiiif/metadata.rb
+++ lib/aspaceiiif/metadata.rb
@@ -2,29 +2,29 @@ require 'aspaceiiif/records'
 
 module ASpaceIIIF
   class Metadata
-    def initialize(resource_id)
-      as_records = Records.new(resource_id)
-      @digital_objects = as_records.digital_objects
-      @digital_object_trees = as_records.digital_object_trees
+    def initialize(dig_obj_id)
+      as_records = Records.new(dig_obj_id)
+      @digital_object = as_records.digital_object
+      @digital_object_tree = as_records.digital_object_tree
       @digital_object_components = as_records.digital_object_components
-      @archival_objects = as_records.archival_objects
+      @archival_object = as_records.archival_object
       @resource = as_records.resource
-      @linked_agents = as_records.linked_agents
+      @linked_agent = as_records.linked_agent
     end
 
     def handle 
-      @digital_objects.map { |record| record["digital_object_id"] }
+      @digital_object["digital_object_id"]
     end
 
     def rights_statement
-      @digital_objects.map { |record| record["notes"].select { |note| note["type"] == "userestrict" }[0]["content"][0] }
+      @digital_object["notes"].select { |note| note["type"] == "userestrict" }[0]["content"][0]
     end
 
     def title
       if host_title.nil?
-        @digital_objects.map { |record| record["title"] }
+        @digital_object["title"]
       else
-        @digital_objects.map { |record| record["title"] + ", " + host_title }
+        @digital_object["title"] + ", " + host_title
       end
     end
 
@@ -37,11 +37,11 @@ module ASpaceIIIF
     end
 
     def component_id
-      @archival_objects.map { |record| record["component_id"] }
+      @archival_object["component_id"]
     end
 
     def creator
-      @linked_agents.map { |record| record["title"] }
+      @linked_agent["title"]
     end
 
     def owner
